aba34854e573fb1781d78896620494cc20bb9dca
TheBlueaa <18384549714@163.com>
2021-01-27 19:16:26 +0800

    分支与循环

--- Basic/BasicPython.py
+++ Basic/BasicPython.py
@@ -1,3 +1,5 @@
+
+
 name = "tom"
 age  = 27
 print("name is ：" +name+ ", age is : " +str(age))
@@ -6,4 +8,12 @@ print("name is : {}, age is : {}".format(name,age))
 
 print("name is ：{0}, age is : {1}".format(age,name))
 print("name is : {n}, age is : {a}".format(n=name,a=age))
+
+print('hello')
+print('''hello''')
+
+print("你说：'今天天气不错'")
+print('你说："今天天气不错"')
+
+
 
---
+++ Basic/BranchingAndCycling.py
@@ -0,0 +1,37 @@
+
+
+a = 2
+b = 3
+if a > b:
+    print(" a max")
+else:
+    print(" b max")
+
+if 2 + 2 == 4:
+    print("你回答正确了")
+else:
+    print("你回答错误了")
+
+s = "hello"
+ss = "hello word"
+if s in ss:
+    print("Contain")
+else:
+    print("Not Contain")
+
+if True:
+    print("True")
+else:
+    print("False")
+
+#成绩
+
+result = 72
+if result >= 90:
+    print("A")
+elif result >= 70:
+    print("b")
+elif result >= 60:
+    print("c")
+else:
+    print("垃圾")
